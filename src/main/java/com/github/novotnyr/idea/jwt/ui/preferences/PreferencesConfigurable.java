212c0a2cf9412dc3132510323d144d922f28c6d5
Robert Novotny <novotnr0@gmail.com>
2020-02-13 23:41:38 +0100

    Validate private and public RSA256 keyfile contents (#16)
    * Synchronize behaviour between public and private key edit fields
    
    * Allow to validate Readers, Strings and Files with private/public keys
    
    * Validate public and private keys in plugin settings

--- src/main/java/com/github/novotnyr/idea/jwt/ui/preferences/PreferencesConfigurable.java
+++ src/main/java/com/github/novotnyr/idea/jwt/ui/preferences/PreferencesConfigurable.java
@@ -1,15 +1,19 @@
 package com.github.novotnyr.idea.jwt.ui.preferences;
 
+import com.github.novotnyr.idea.jwt.rs256.RsaUtils;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.TextBrowseFolderListener;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import java.io.File;
 
 public class PreferencesConfigurable implements Configurable {
     private final Project project;
@@ -46,10 +50,33 @@ public class PreferencesConfigurable implements Configurable {
     }
 
     @Override
-    public void apply() {
-        PluginPreferences pluginPreferences = PluginPreferences.getInstance(project);
-        pluginPreferences.setRs256PublicKeyFile(this.rs256PublicKeyTextField.getText());
-        pluginPreferences.setRs256PrivateKeyFile(this.rs256PrivateKeyTextField.getText());
+    public void apply() throws ConfigurationException {
+        try {
+            validateRs256PublicKeyFile();
+            validateRs256PrivateKeyFile();
+
+            PluginPreferences pluginPreferences = PluginPreferences.getInstance(project);
+            pluginPreferences.setRs256PublicKeyFile(this.rs256PublicKeyTextField.getText());
+            pluginPreferences.setRs256PrivateKeyFile(this.rs256PrivateKeyTextField.getText());
+        } catch (Exception e) {
+            throw new ConfigurationException(e.getMessage());
+        }
+    }
+
+    private void validateRs256PrivateKeyFile() {
+        String rs256PrivateKey = this.rs256PrivateKeyTextField.getText();
+        if (StringUtil.isEmpty(rs256PrivateKey)) {
+            return;
+        }
+        RsaUtils.getPrivateKey(rs256PrivateKey);
+    }
+
+    private void validateRs256PublicKeyFile() {
+        String rs256PublicKey = this.rs256PublicKeyTextField.getText();
+        if (StringUtil.isEmpty(rs256PublicKey)) {
+            return;
+        }
+        RsaUtils.getPublicKey(new File(rs256PublicKey));
     }
 
 }
