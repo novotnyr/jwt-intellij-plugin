0d3f8db40e1e4ef2f734195d0bc55f4923d9be03
Robert Novotny <novotnr0@gmail.com>
2024-01-09 19:13:02 +0100

    Use compatible method to retrieve ContentFactory instance

--- src/main/java/com/github/novotnyr/idea/jwt/ToolWindowFactory.java
+++ src/main/java/com/github/novotnyr/idea/jwt/ToolWindowFactory.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.ActionGroup;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.actionSystem.ex.CheckboxAction;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
@@ -18,7 +19,7 @@ public class ToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFact
         JwtExplorer jwtExplorer = new JwtExplorer();
         jwtExplorer.setProject(project);
 
-        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        ContentFactory contentFactory = getContentFactory();
         Content content = contentFactory.createContent(jwtExplorer, "", false);
         toolWindow.getContentManager().addContent(content);
 
@@ -28,6 +29,13 @@ public class ToolWindowFactory implements com.intellij.openapi.wm.ToolWindowFact
         }
 
         content.setDisposer(jwtExplorer);
+    }
+
+    private static ContentFactory getContentFactory() {
+        // IC-2022.1 introduced ContentFactory.getInstance() and deprecated ContentFactory.SERVICE.getInstance()
+        // Use implementation from the IC sources until we increase the 'since' compatibility
+        // https://github.com/JetBrains/intellij-community/blame/ca1151aa9f88e1e9f727d55736916cbe48d9518b/platform/ide-core/src/com/intellij/ui/content/ContentFactory.java#L27
+        return ApplicationManager.getApplication().getService(ContentFactory.class);
     }
 
     private ActionGroup getToolWindowActionGroup() {
