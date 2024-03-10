bb74943d4bf8d717e3b2d9bf203f61e06b0207b4
Robert Novotny <novotnr0@gmail.com>
2024-03-10 23:20:47 +0100

    Refer to snapshot Maven Central repo and Gradle Plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "jwt-intellij-plugin"
+pluginManagement {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        gradlePluginPortal()
+    }
+}
