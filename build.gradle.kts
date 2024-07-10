6cf65c1a6d99a4cd1abf395e4954ff1fb29f5269
Turan <11923689+TuranDev@users.noreply.github.com>
2024-07-10 23:13:23 +0100

    Bump kacson databin version to remove vulnerability

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.8.4")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     implementation("com.auth0:java-jwt:3.2.0")
     implementation("org.ocpsoft.prettytime:prettytime:4.0.2.Final")
     implementation("org.bouncycastle:bcpkix-jdk15on:1.61")
