38388b885fbe4ac43dbd53d88040bb78ea63648c
Robert Novotny <novotnr0@gmail.com>
2026-04-05 00:58:42 +0200

    Use constants for claim names

--- src/main/java/com/github/novotnyr/idea/jwt/core/JwtFactory.java
+++ src/main/java/com/github/novotnyr/idea/jwt/core/JwtFactory.java
@@ -4,6 +4,10 @@ import com.github.novotnyr.idea.jwt.SignatureContext;
 
 import java.util.Date;
 
+import static com.auth0.jwt.HeaderParams.ALGORITHM;
+import static com.auth0.jwt.HeaderParams.TYPE;
+import static com.auth0.jwt.RegisteredClaims.ISSUED_AT;
+
 public class JwtFactory {
     private static JwtFactory INSTANCE = new JwtFactory();
 
@@ -19,10 +23,10 @@ public class JwtFactory {
         Jwt jwt = new Jwt();
         jwt.setAlgorithm(algorithm);
         jwt.setSignatureContext(signatureContext);
-        jwt.setHeaderClaim(new StringClaim("alg", algorithm));
-        jwt.setHeaderClaim(new StringClaim("typ", "JWT"));
+        jwt.setHeaderClaim(new StringClaim(ALGORITHM, algorithm));
+        jwt.setHeaderClaim(new StringClaim(TYPE, "JWT"));
         if (addIat) {
-            jwt.setPayloadClaim(new DateClaim("iat", new Date()));
+            jwt.setPayloadClaim(new DateClaim(ISSUED_AT, new Date()));
         }
         jwt.rebuild();
         return jwt;
