cc6d37a4f18c3ed64fee8bbe80425d6f43230b80
Robert Novotny <novotnr0@gmail.com>
2026-04-05 00:49:13 +0200

    Bump Jackson to 2.21.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     implementation("org.jspecify:jspecify:1.0.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.8.4")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.21.2")
     implementation("com.auth0:java-jwt:4.5.1")
     implementation("org.ocpsoft.prettytime:prettytime:4.0.2.Final")
     implementation("org.bouncycastle:bcpkix-jdk15on:1.61")
